c2391ef719b3467a59eac14c26023e87592b3dea
imjesusleal <jesusalbertolv@gmail.com>
2022-09-29 16:29:52 -0300

    Index, falta footer

--- proyecto_nocountry/proyecto_nocountry/urls.py
+++ proyecto_nocountry/proyecto_nocountry/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+from mainapp import urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('', include('mainapp.urls'))
 ]
