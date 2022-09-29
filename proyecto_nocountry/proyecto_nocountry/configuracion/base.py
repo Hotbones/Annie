fb9e0dba12f98b991aaf3657704970aa886c1e02
Santiago Muñoz <san88.zzl.308@gmail.com>
2022-09-29 16:34:19 -0300

    DB actualizada 1

--- proyecto_nocountry/proyecto_nocountry/configuracion/base.py
+++ proyecto_nocountry/proyecto_nocountry/configuracion/base.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'una-secret-key'
 
 
 # Application definition
