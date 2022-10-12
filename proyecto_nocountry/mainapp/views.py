760b35e44d318df68440ce5f326e878ae68e3546
Santiago Muñoz <san88.zzl.308@gmail.com>
2022-10-12 20:25:34 -0300

    view para crear mensajes y customizacion de template index y perfilcliente/niñera

--- proyecto_nocountry/mainapp/views.py
+++ proyecto_nocountry/mainapp/views.py
@@ -7,17 +7,33 @@ from .forms import NiñeraForm,ClienteForm,RegisterForm,MensajeForm
 from .models import *
 
 def index(request):
-    return render(request, 'mainapp/index.html', {})
+    perfil=None
+    cliente=None
+    niñera=None
+    try:
+        perfil = Niñera.objects.get(perfil_id=request.user.id)
+        niñera = perfil
+    except:
+        pass
+    try:
+        perfil = Cliente.objects.get(perfil_id=request.user.id)
+        cliente = perfil
+    except:
+        pass
+    context = {'perfil':perfil,'niñera':niñera,'cliente':cliente}
+    return render(request, 'mainapp/index.html', context)
 
 def searcher(request):
     return render(request, 'mainapp/searcher.html', {})
 
+@login_required(login_url='logueo')
 def perfil_niñera(request):
     niñeras = Niñera.objects.all()
-
-    context = {'niñeras':niñeras}
+    mi_perfil = Niñera.objects.get(perfil_id=request.user.id)
+    context = {'niñeras':niñeras, 'mi_perfil':mi_perfil}
     return render(request, 'mainapp/perfilniñera.html', context)
 
+@login_required(login_url='logueo')
 def perfil_cliente(request):
     return render(request, 'mainapp/perfilcliente.html', {})
 
@@ -71,8 +87,8 @@ def register_niñera(request,user):
     if not request.user.is_authenticated:
         messages.error(request,'No se puede ir a la direccion')
         return redirect('index')
-    if Niñera.objects.filter(perfil_niñera=request.user).exists() or \
-        Cliente.objects.filter(perfil_cliente=request.user).exists():
+    if Niñera.objects.filter(perfil=request.user).exists() or \
+        Cliente.objects.filter(perfil=request.user).exists():
         return redirect('index')
     
     form = NiñeraForm()
@@ -83,9 +99,9 @@ def register_niñera(request,user):
         if form.is_valid():
             form = form.save(commit=False)
             user = User.objects.get(username = request.user.username)
-            form.perfil_niñera = user
+            form.perfil = user
 
-            if form.perfil_niñera not in Niñera.objects.all():
+            if form.perfil not in Niñera.objects.all():
                 form.save()
                 messages.success(request, message='Registro como niñera exitoso!')
                 return redirect('index')  # access granted
@@ -103,8 +119,8 @@ def register_niñera(request,user):
 @login_required(login_url='logueo')
 def register_cliente(request,user):
    
-    if Niñera.objects.filter(perfil_niñera=request.user).exists() or \
-    Cliente.objects.filter(perfil_cliente=request.user).exists():
+    if Niñera.objects.filter(perfil=request.user).exists() or \
+    Cliente.objects.filter(perfil=request.user).exists():
         messages.error(request, message='Ya has creado un perfil')
         return redirect('index')
 
@@ -119,9 +135,9 @@ def register_cliente(request,user):
         if form.is_valid():
             form = form.save(commit=False)
             user = User.objects.get(username = request.user.username)
-            form.perfil_cliente = user
+            form.perfil = user
             # if not Cliente.objects.all():
-            if form.perfil_cliente not in Cliente.objects.all():
+            if form.perfil not in Cliente.objects.all():
                 form.save()
                 messages.success(request, message='Registro como cliente exitoso!')
                 return redirect('index')
@@ -170,9 +186,6 @@ def crear_mensaje(request,pk):
     puntaje=0
     usuario = User.objects.get(id=pk)
 
-    # mensajes = Mensaje.usuario_id.mensaje_set.all() #message es el model, esta es la forma de pedir el set de messages que estan referidos a cierto room
-
-
     form = MensajeForm(instance=usuario)
     if request.method == 'POST':
         form = MensajeForm(request.POST, instance=usuario)
@@ -180,7 +193,7 @@ def crear_mensaje(request,pk):
             comentarista = request.user.username
             puntaje=str(puntaje)
             nuevo_comentario = form.cleaned_data['mensaje']
-            c = Mensaje(usuario=usuario, comentarista=comentarista, mensaje=nuevo_comentario, puntaje=puntaje)
+            c = Mensaje(usuario=usuario, comentarista=comentarista, puntaje=puntaje, mensaje=nuevo_comentario)
             c.save()
 
             return redirect('index')
